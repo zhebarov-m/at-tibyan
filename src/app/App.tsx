7b6aa1611284db6a4476d3963589b91b8811236a
xarcho.ev <xarcho.ev@gmail.com>
2024-03-27 20:21:09 +0300

    added FSD and tailwind to the project

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import './styles/App.css'
+
+function App() {
+
+
+  return (
+    <div  className="underline">
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import App from './app/App.tsx'
+import './app/styles/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <App />
+)
