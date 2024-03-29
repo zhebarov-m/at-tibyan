c0ca0401e806dc37b13e35d141a91e634b500c61
xarcho.ev <xarcho.ev@gmail.com>
2024-03-29 17:08:55 +0300

    добавил несколько новых медиа-запросов в конфиг

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,31 @@ export default {
         'sura': '0 5x 10px 0 rgb(23 23 23 / 19%)',
       }
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'sd': '600px',
+      // => @media (min-width: 699px) { ... }
+
+      'xr': '699px',
+      // => @media (min-width: 699px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'mz': '932px',
+      // => @media (min-width: 1024px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
