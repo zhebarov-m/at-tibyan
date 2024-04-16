3409551d13872bb87a567584922e3744167ef65e
Abdullah <lechiev2015@mail.ru>
2024-04-16 16:41:00 +0300

    feat: добавил кнопки для сур и вынес типы пропсов в AyahList.tsx

---
+++ src/shared/ui/Buttton/Button.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: "small" | "medium" | "large";
+}
+
+const Button: React.FC<ButtonProps> = ({
+  className,
+  size = "medium",
+  ...props
+}) => {
+  return (
+    <button
+      className={`rounded-lg focus:outline-none transition-colors duration-300 ease-in-out border border-gray-300 ${
+        size === "small"
+          ? "py-1 px-2 text-xs"
+          : size === "large"
+          ? "py-3 px-6 text-lg"
+          : "py-1 px-3 text-base"
+      } hover:bg-[#009c9c] hover:text-white ${className} dark:bg-gray-800 dark:hover:bg-[#009c9c] dark:text-white`}
+      {...props}
+    ></button>
+  );
+};
+
+export default Button;
--- src/widgets/AyahsList/ui/AyahsList.tsx/AyahsList.tsx
+++ src/widgets/AyahsList/ui/AyahsList.tsx/AyahsList.tsx
@@ -2,25 +2,50 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { ISura } from "@/entities/Ayahs/model/types/ayahs";
 import Ayahs from "@/entities/Ayahs/ui/Ayahs";
+import Button from "@/shared/ui/Buttton/Button";
 
 const AyahsList = () => {
   const { id } = useParams();
   const [ayahs, setAyahs] = useState<ISura[]>([]);
   const [allSura, _] = useState(false);
+  const [currentSurah, setCurrentSurah] = useState(Number(id));
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   useEffect(() => {
+    fetchSurah(currentSurah);
+  }, [currentSurah]);
+
+  const fetchSurah = (surahNumber: number) => {
     fetch(
-      `https://api.alquran.cloud/v1/surah/${id}/editions/quran-uthmani,ru.kuliev`
+      `https://api.alquran.cloud/v1/surah/${surahNumber}/editions/quran-uthmani,ru.kuliev`
     )
       .then((res) => res.json())
       .then((data) => {
         setAyahs(data.data);
       });
-  }, [id]);
+  };
+
+  const goToNextSurah = () => {
+    if (!isDisabledNextSura) {
+      const nextSurahNumber = currentSurah + 1;
+      setCurrentSurah(nextSurahNumber);
+      fetchSurah(nextSurahNumber);
+    }
+  };
+
+  const goToPreviousSurah = () => {
+    if (!isDisabledPreviousSura) {
+      const previousSurahNumber = currentSurah - 1;
+      setCurrentSurah(previousSurahNumber);
+      fetchSurah(previousSurahNumber);
+    }
+  };
+
+  const isDisabledNextSura: boolean = currentSurah === 114;
+  const isDisabledPreviousSura: boolean = currentSurah === 1;
 
   return (
     <div>
@@ -29,9 +54,39 @@ const AyahsList = () => {
           className={allSura ? "styles.modeAllSura" : "styles.modeLearnSura"}
         >
           {ayahs[0]?.ayahs.map((aya, index) => {
-            return <Ayahs key = {aya.number} index ={index} aya={aya} allSura={allSura} ayahs = {ayahs}/>;
+            return (
+              <Ayahs
+                key={aya.number}
+                index={index}
+                aya={aya}
+                allSura={allSura}
+                ayahs={ayahs}
+              />
+            );
           })}
         </div>
+      </div>
+      <div className="flex justify-center items-center mt-8 ">
+        <Button
+          onClick={goToPreviousSurah}
+          className={`mr-4 ${
+            isDisabledPreviousSura &&
+            "bg-gray-300 text-gray-400 cursor-not-allowed pointer-events-none"
+          }`}
+        >
+          <span className="mr-2">&#10094;</span>
+          Предыдущая сура
+        </Button>
+        <Button
+          onClick={goToNextSurah}
+          className={`${
+            isDisabledNextSura &&
+            "bg-gray-200 text-gray-400 cursor-not-allowed pointer-events-none"
+          }`}
+        >
+          Следующая сура
+          <span className="ml-2">&#10095;</span>
+        </Button>
       </div>
     </div>
   );
